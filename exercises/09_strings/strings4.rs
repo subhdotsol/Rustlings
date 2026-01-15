689ac2f5fd157e65f2d9fc64342758fc946a2696
subh <subhajitchaudhury05@gmail.com>
2026-01-15 12:35:38 +0530

    fix : strings done

--- exercises/09_strings/strings4.rs
+++ exercises/09_strings/strings4.rs
@@ -1,5 +1,5 @@
 // Calls of this function should be replaced with calls of `string_slice` or `string`.
-fn placeholder() {}
+fn placeholder(arg: &str) {}
 
 fn string_slice(arg: &str) {
     println!("{arg}");
@@ -13,25 +13,25 @@ fn string(arg: String) {
 // Your task is to replace `placeholder(…)` with either `string_slice(…)`
 // or `string(…)` depending on what you think each value is.
 fn main() {
-    placeholder("blue");
+    placeholder(&"blue");
 
-    placeholder("red".to_string());
+    placeholder(&"red".to_string());
 
-    placeholder(String::from("hi"));
+    placeholder(&"hi".to_string());
 
-    placeholder("rust is fun!".to_owned());
+    placeholder(&"rust is fun!".to_owned());
 
     placeholder("nice weather".into());
 
-    placeholder(format!("Interpolation {}", "Station"));
+    placeholder(&format!("Interpolation {}", "Station"));
 
     // WARNING: This is byte indexing, not character indexing.
     // Character indexing can be done using `s.chars().nth(INDEX)`.
     placeholder(&String::from("abc")[0..1]);
 
-    placeholder("  hello there ".trim());
+    placeholder(&"  hello there ".trim());
 
-    placeholder("Happy Monday!".replace("Mon", "Tues"));
+    placeholder(&"Happy Monday!".replace("Mon", "Tues"));
 
-    placeholder("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    placeholder(&"mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
