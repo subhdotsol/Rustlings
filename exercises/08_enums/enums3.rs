3571c4178df98512f558349417d7a38e801a2fef
subh <subhajitchaudhury05@gmail.com>
2026-01-15 11:49:19 +0530

    fix : enums done

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -16,7 +16,6 @@ struct State {
     height: u64,
     position: Point,
     message: String,
-    // RGB color composed of red, green and blue.
     color: (u8, u8, u8),
     quit: bool,
 }
@@ -45,6 +44,14 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
+        match message {
+            Message::Resize { width, height } => self.resize(width, height),
+            Message::Move(point) => self.move_position(point),
+            Message::Echo(s) => self.echo(s),
+            Message::ChangeColor(red, green, blue) => self.change_color(red, green, blue),
+            Message::Quit => self.quit(),
+        }
+
         // variants using the methods defined above.
     }
 }
